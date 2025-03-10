1ab3c5be5f32c957ea8e81f7cd8022c2741e999a
The1Swan N/A <isabella.ann.swan@gmail.com>
2025-03-10 17:51:49 +0000

    rock, paper, scissors still in process

--- task.py
+++ task.py
@@ -1,52 +1,73 @@
 import random
 
-moves = [rock, paper, scissors, blaster]
-game_master = moves[random.randint(0,2)]
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+blaster = "blaster"
 
-def rock():
+moves = [rock, paper, scissors, blaster]
+
+def rock(player, game_master):
     if player == rock and game_master == scissors:
         print("player wins")
     elif player == rock and game_master == paper:
         print("game_master wins")
     elif player == rock and game_master == rock:
         print("tie")
+    elif player == rock and game_master == blaster:
+        print("game_master wins")
 
-def paper():
+def paper(player, game_master):
     if player == paper and game_master == scissors:
         print("game_master wins")
     elif player == paper and game_master == paper:
         print("tie")
     elif player == paper and game_master == rock:
         print("player wins")
+    elif player == paper and game_master == blaster:
+        print("player wins")
 
-def scissors():
+def scissors(player, game_master):
     if player == scissors and game_master == scissors:
         print("tie")
     elif player == scissors and game_master == paper:
-        print("paper wins")
+        print("player wins")
     elif player == scissors and game_master == rock:
         print("game_master wins")
+    elif player == scissors and game_master == blaster:
+        print("game_master wins")
 
-def blaster():
-    if player == blaster:
+def blaster(player, game_master):
+    if player == blaster and game_master == scissors:
         print("player wins")
+    elif player == blaster and game_master == paper:
+        print("player wins")
+    elif player == blaster and game_master == rock:
+        print("player wins")
+    elif player == blaster and game_master == blaster:
+        print("tie")
+
 
 def play_game():
-    player = True
-
-    while player == True:
-        player = input("whats your move?")
-        if player == "rock":
-            rock()
-        elif player == "paper":
-            paper()
-        elif player == "scissors":
-            scissors()
-        elif player == "blaster":
-            blaster()
-        else:
-            print("errror")
+    while True:
+        player = input("What's your move? ")
         
-        play_again = input("Do you want to play again")
+        game_master = random.choice(moves(0,2))
+
+        print("Game master chose" + game_master)
+
+        if player == "rock":
+            rock(player, game_master)
+        elif player == "paper":
+            paper(player, game_master)
+        elif player == "scissors":
+            scissors(player, game_master)
+        elif player == "blaster":
+            blaster(player, game_master)
+        
+        play_again = input("Do you want to play again? ")
         if play_again == "no":
+            print("Thanks for playing!")
             break
+
+play_game()
